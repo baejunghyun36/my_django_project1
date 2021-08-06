12b39a689c45798b080596c3cfdfed9c8bffeeeb
배정현 <bae1004kin@likelion.org>
2021-08-06 16:14:12 +0900

    author 필드 추가

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
+
+    hook_text =models.CharField(max_length=100, blank=True)
+    
     content = models.TextField()
     
     head_image=models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
@@ -14,6 +18,9 @@ class Post(models.Model):
     
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
