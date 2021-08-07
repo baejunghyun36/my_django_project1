4f7b02c5f09994453b0fbab0390349c0ee64aa4b
배정현 <bae1004kin@likelion.org>
2021-08-07 22:26:51 +0900

    가족사진 올리기

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
 
 # Register your models here.
-from .models import Post
+from .models import Comment, Post
 admin.site.register(Post, MarkdownxModelAdmin)
+admin.site.register(Comment)
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
 
     path('', include('single_pages.urls')),
     path('markdownx/', include('markdownx.urls')),
+    path('accounts/', include('allauth.urls')),
     
 ] 
 
